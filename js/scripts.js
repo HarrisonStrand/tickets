32e5bce448b1fde984d277887dd4441002a02a67
HarrisonStrand <harrisonstrand@gmail.com>
2020-11-03 16:33:35 -0800

    start function for new releases vs matinee movies
    Co-authored-by: Kevin Kirkley <kevinmkirkley@gmail.com>
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@
 function TicketPrice() {
   this.tickets = [];
   this.currentId = 0;
+  const newRelease = "Tenet";
 }
   
 TicketPrice.prototype.addTicket = function(ticket) {
@@ -16,36 +17,53 @@ TicketPrice.prototype.assignId = function() {
   return this.currentId;
 }
 
-//Ticket logic
-// let MovieName = new MovieName
-// function MovieName(newRelease, oldRelease) {
-//   this.newRelease = newRelease;
-//   this.oldRelease = oldRelease;
-// }
+TicketPrice.prototype.findTicket = function(id) {
+  for (let i=0; i< this.tickets.length; i++) {
+    if (this.tickets[i]) {
+      if (this.tickets[i].id == id) {
+        
+        return this.tickets[i];
+      }
+    }
+  };
+  return false;
+}
+
+
+
 function Ticket(movieName, movieTime, attendeeAge) {
   this.movieName = movieName;
   this.movieTime = movieTime;
   this.attendeeAge = attendeeAge;
 }
 
-// Ticket.prototype.description = function() {
-//   return this.movieName + this.movieTime + this.attendeeAge;
+// Ticket.prototype.movieNameIdentifier = function() {
+//   for(let i = 0; i <=movieName.length; i ++) {
+//     if(this.movieName[i] == "Step Brothers")
+//     console.log("Hi");
+//   }
 // }
+
+
 
 
 //UI Logic
 let newTicketPrice = new TicketPrice();
+
 $(document).ready(function(event) {
   $("button#submit").click(function() {
-    let inputtedAge = $("input#age").val();   
     let inputtedMovie = $("select#movie-title").val();
     let inputtedTime = $("select#movie-time").val();
+    let inputtedAge = $("input#age").val();   
     
     let newTicket = new Ticket(inputtedMovie, inputtedTime, inputtedAge);
     newTicketPrice.addTicket(newTicket);
     console.log(newTicketPrice);
     
-    
+    let selectedMovie = newTicketPrice.findTicket(0);
+    if(selectedMovie.newRelease === true) {
+      totalCost += 5;
+    }
     
   });
 });
